845797e7dc4f410814b95e510ec76aba5f166ee5
Dan Becker <dabecker@paypal.com>
2015-06-04 14:24:16 -0500

    Arquilian changes

--- src/test/java/com/example/rest/example/services/BookServiceTest.java
+++ src/test/java/com/example/rest/example/services/BookServiceTest.java
@@ -1,7 +1,22 @@
 package com.example.rest.example.services;
 
+import java.net.URL;
+import java.net.URI;
+import java.net.MalformedURLException;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.OperateOnDeployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+
 @RunWith(Arquillian.class)
-public class BookTest {
+public class BookServiceTest {
 
     @Deployment(testable = false, name = "bookservice")
     public static WebArchive createDeploymentBookInfoService() {
